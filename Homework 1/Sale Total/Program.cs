33fce2967992911b4a95c5b1280d61fc150f4681
Lauren Watson <l.watson@ou.edu>
2019-09-05 14:11:29 -0500

    Finished homework 1

---
+++ Homework 1/Sale Total/Program.cs
@@ -0,0 +1,42 @@
+﻿// @author Lauren Watson
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sale_Total
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string productNameAsString;
+            double salesPrice;
+            int quantity;
+
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            productNameAsString = Console.ReadLine();
+
+            Console.WriteLine($"How many {productNameAsString}'s do you want to buy?");
+            quantity = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine($"What is the price for each {productNameAsString}?");
+            salesPrice = Convert.ToDouble(Console.ReadLine());
+
+            const double tax = .085;
+            double subTotal = quantity * salesPrice;
+            double totalTax = subTotal * tax;
+            double totalAmount = subTotal + totalTax;
+
+            Console.WriteLine($"\tYour subtotal for your bill is {subTotal.ToString("C2")}.");
+            Console.WriteLine($"\tYour sales tax for your bill is {totalTax.ToString("C2")}.");
+            Console.WriteLine($"\tYour total for your bill is {totalAmount.ToString("C2")}.");
+
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework 1/Student Grade/Program.cs
@@ -0,0 +1,43 @@
+﻿// @author Lauren Watson
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, lastName;
+            int studentId;
+            double h, p, q, m, f, finalGrade;
+
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+            Console.WriteLine("What is your student id?");
+            studentId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            q = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for the midterm?");
+            m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your overall percentage grade for the final?");
+            f = Convert.ToDouble(Console.ReadLine());
+
+            finalGrade = (p / 100 * .10) + (h / 100 * .25) + (q / 100 * .1) + (m / 100 * .25) + (f / 100 * .3);
+
+            Console.WriteLine(firstName + " " + lastName + " " + "(" + studentId + ")," + $" your final grade is {finalGrade.ToString("P2")}.");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
